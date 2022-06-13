653e79610a78a92e768535e0f3073f3fb882a460
iagoa <iagoantunes.f@gmail.com>
2022-06-12 21:58:44 -0300

    Add Controller

---
+++ MVCCourse/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCourse.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public String Index()
+        {
+            return "This is the Index Page";
+        }
+
+        public String Hello()
+        {
+            return "Who's there";
+        }
+    }
+}
