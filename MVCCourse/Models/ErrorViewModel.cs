64b61cfa7b0a972569a5e3c890ecbb00bb5563fb
iagoa <iagoantunes.f@gmail.com>
2022-06-12 21:52:28 -0300

    Adicionar arquivos de projeto.

---
+++ MVCCourse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCourse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
